af28ec8a8ea6a65bab079becae178204315b38e2
j0hanz <golfarenlj@gmail.com>
2026-03-11 19:43:39 +0100

    fix: add suppressHydrationWarning to RootLayout for improved hydration handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={geistSans.variable}>
+    <html lang="en" className={geistSans.variable} suppressHydrationWarning>
       <body className={geistMono.variable}>
         <InitColorSchemeScript />
         <AppRouterCacheProvider>
