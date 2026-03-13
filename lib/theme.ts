a2ba4ccf7a29cc5a60e5c2c898e5ef5997cc6c44
j0hanz <golfarenlj@gmail.com>
2026-03-13 14:16:04 +0100

    refactor: enhance theme configuration with light and dark color schemes

--- lib/theme.ts
+++ lib/theme.ts
@@ -5,11 +5,38 @@ import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 const theme = responsiveFontSizes(
   createTheme({
     cssVariables: { colorSchemeSelector: "class" },
-    colorSchemes: { dark: true },
+    colorSchemes: {
+      light: {
+        palette: {
+          background: {
+            default: "#FFFFFF",
+            paper: "rgba(255, 255, 255, 0.8)",
+          },
+        },
+      },
+      dark: {
+        palette: {
+          background: {
+            default: "#393939",
+            paper: "rgba(57, 57, 57, 0.74)",
+          },
+        },
+      },
+    },
     typography: {
       fontFamily: "var(--font-geist-sans)",
     },
     components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: {
+            borderRadius: 16,
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            backdropFilter: "blur(12.3px)",
+            WebkitBackdropFilter: "blur(12.3px)",
+          },
+        },
+      },
       MuiOutlinedInput: {
         styleOverrides: {
           input: {
