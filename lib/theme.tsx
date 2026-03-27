e1d7b371cd2aefc6d13ae940c462361d7f4f28e0
j0hanz <golfarenlj@gmail.com>
2026-03-27 14:19:37 +0100

    feat: update toggleButton styles to improve layout consistency

--- lib/theme.tsx
+++ lib/theme.tsx
@@ -145,7 +145,7 @@ export const sx = {
     textOverflow: 'ellipsis',
     maxWidth: responsive.truncateWidth,
   },
-  toggleButton: { border: 0 },
+  toggleButton: { border: 0, minWidth: 64, px: 1, py: 0.5 },
   headerButton: {
     justifyContent: 'flex-start',
     textAlign: 'left',
